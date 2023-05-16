1960de630e47bb9161378f82baf325749a83764e
Victoriano <59400167+V-Juarez@users.noreply.github.com>
2023-05-15 22:56:03 -0600

    while statement

---
+++ Thurday/while_statement.js
@@ -0,0 +1,29 @@
+let text = ""
+let i = 0
+
+while (i <= 10) {
+  text += 'The number is: ' + i + '\n'
+  i++
+}
+console.log(text, i)
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let j = 0;
+let car = "";
+
+while (cars[j]) {
+  car += cars[j] + '\n';
+  j++;
+}
+console.log(car)
+
+let num1 = 0
+let msg = ''
+
+while (num1 < 20) {
+  msg += 'The number is ' + num1 + '\n'
+  num1++
+}
+
+console.log(msg)
+
