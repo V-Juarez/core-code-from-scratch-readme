dd8965622aa8e31d159b8035b6413d439588d0bf
Victoriano <59400167+V-Juarez@users.noreply.github.com>
2023-05-15 22:16:55 -0600

    🚧 work if

---
+++ Thurday/if_statement.js
@@ -0,0 +1,14 @@
+// Greeting day
+// Create if statement for day, morning or evening
+// morning => Good day
+// evening => Good Evening
+
+let hour = 22
+
+if (hour < 18) {
+  greeting = "Good day"
+  console.log(greeting)
+} else {
+  greeting = "Good evening"
+  console.log(greeting)
+}
