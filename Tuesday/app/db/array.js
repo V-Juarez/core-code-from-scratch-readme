28c566d7b7a8d1281d9406150e9b3cea6204e8b1
Victoriano <mytacam78@gmail.com>
2023-06-21 08:00:25 -0700

    🦺 add user on db

--- Tuesday/app/db/array.js
+++ Tuesday/app/db/array.js
@@ -1,3 +1,7 @@
-const db = []
+const db = [
+  { id: 1, name: "Leonardo", age: 22 },
+  { id: 2, name: "Manuel", age: 21 },
+  { id: 3, name: "Messi", age: 35 },
+]
 
 module.exports = db
--- Tuesday/app/routes/index.js
+++ Tuesday/app/routes/index.js
@@ -1,22 +1,54 @@
-const { Router } = require('express')
-const router = Router()
+const { Router } = require("express");
+const router = Router();
+const db = require("../db/array");
 
-router.get('/user', (request, response, next) => {
-  response.status(200).json({ message: 'get'})
-})
+router.get("/user", (request, response, next) => {
+  response.status(200).json({
+    message: "Lists Users",
+    data: db,
+  });
+});
 
-router.post('/user', (request, response, next) => {
-  response.status(200).json({ message: 'post'})
-})
+router.post("/user", (request, response, next) => {
+  // destructor Js
+  try {
+    const { name, age } = request.body;
+    const id = db.length + 1;
+    const newUser = {
+      id: db.length === 0 ? 1 : db.length,
+      name,
+      age,
+    };
+    db.push(newUser);
+    response.status(200).json({ message: "Create new user", user: newUser });
+  } catch (error) {
+    response.status(500).json({ message: "hay un error de servidor" });
+  }
+});
 
-router.put('/user', (request, response, next) => {
-  response.status(200).json({ message: 'put'})
-})
+router.put("/user", (request, response, next) => {
+  // validation user
+  const { id, name, age } = request.body;
+  const userSelect = db.filter((user) => user.id === id);
+  if (userSelect.length === 0) {
+    return response
+      .status(200)
+      .json({ message: "no existe el usuario en la db" });
+  }
 
-router.delete('/user', (request, response, next) => {
-  response.status(200).json({ message: 'delete'})
-})
+  // edit user
+  db.map((user) => {
+    if (user.id === id) {
+      user.name = name;
+      user.age = age
+    }
+    return user
+  })
+  response.status(200).json({ message: "Edit user" });
+});
 
+router.delete("/user", (request, response, next) => {
+  response.status(200).json({ message: "delete" });
+});
 
-
-module.exports = router
+module.exports = router;
