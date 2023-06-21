31bbf03730e115f4f7ce5cc648a1c39cd84c3815
Victoriano <mytacam78@gmail.com>
2023-06-21 07:25:32 -0700

    🗃️ add array db

--- Tuesday/app/routes/index.js
+++ Tuesday/app/routes/index.js
@@ -1,19 +1,19 @@
 const { Router } = require('express')
 const router = Router()
 
-router.get('/', (request, response, next) => {
+router.get('/user', (request, response, next) => {
   response.status(200).json({ message: 'get'})
 })
 
-router.post('/', (request, response, next) => {
+router.post('/user', (request, response, next) => {
   response.status(200).json({ message: 'post'})
 })
 
-router.put('/', (request, response, next) => {
+router.put('/user', (request, response, next) => {
   response.status(200).json({ message: 'put'})
 })
 
-router.delete('/', (request, response, next) => {
+router.delete('/user', (request, response, next) => {
   response.status(200).json({ message: 'delete'})
 })
 
