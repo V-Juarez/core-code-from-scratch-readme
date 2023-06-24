367083c2663f3d4564a48af012f7a736afe6806b
Victoriano <mytacam78@gmail.com>
2023-06-23 17:57:46 -0700

    🦺 complete crud

--- Wednesday/createUser/index.js
+++ Wednesday/createUser/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const morgan = require('morgan')
+// const cors = require('cors')
+
+// app.use(cors())
 const app = express()
+const routes = require('./routes/index')
 
-app.use()
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use((request, response, next) => {
+  console.log(request.url, request.method);
+  next()
+})
+
+app.use('/api', routes)
 
 
-app.listen(4001, () => {
-  console.log('Listen server on port 4001');
+app.listen(4000, () => {
+  console.log('Server listen port 4000');
 })
--- Wednesday/standUp/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use()
-
-
-app.listen(4001, () => {
-  console.log('Listen server on port 4001');
-})
