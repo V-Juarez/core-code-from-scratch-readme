0741d6accd2a5799834932f99e06b5c2ebe7a4e0
Victoriano <mytacam78@gmail.com>
2023-06-25 08:04:03 -0700

    🗃️ create db and route get

---
+++ Thursday/standUp/services/db.js
@@ -0,0 +1,44 @@
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const dbPath = path.resolve(process.cwd(), "services", "testing.sqlite");
+// console.log(dbPath);
+
+const db = new sqlite3.Database(dbPath);
+
+// db.serialize(() => {
+//   db.run('CREATE TABLE users (id INT, name TEXT, age INT)')
+//   db.run('INSERT INTO users VALUES  (1, "Leonardo", 78)')
+//   db.run('SELECT * FROM users', (error) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//   })
+// })
+
+const get = (query, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.all(query, params, (err, rows) => {
+      if (err) {
+        console.log(err)
+        return reject(err);
+      }
+      resolve(rows);
+    });
+  });
+};
+
+const  initDB = async () => {
+  try {
+    await db.run('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)')
+    console.log('Tabla users Cargadas')
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+
+module.exports = {
+  get,
+  initDB
+};
