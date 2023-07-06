64ecc0772f17749eac4877bfd4bc1a7d3321e3d6
Victoriano <mytacam78@gmail.com>
2023-07-06 09:05:21 -0600

    🗃️ conection db

--- Backend/routes/task.js
+++ Backend/routes/task.js
@@ -1,14 +1,32 @@
 const { Router } = require('express')
+const moment = require('moment')
 const router = Router()
 
 // data
-const { get } = require('./../services/db')
+const db  = require('./../services/db')
 const validatorData = require('./../middlewares/validatorData')
 
 router.get('/', (req, res, next) => {
+  const taskId = req.query.id
   console.log(
     'peticion get'
   );
+  db.get("SELECT * FROM tasks WHERE id = ?", [taskId])
+    .then((data) => {
+      const dataFormatted = data.map((item) => {
+        return {
+          title: item['title'],
+          desription: item['description'],
+          isdone: item['isdone'],
+          id: item['id'],
+          created_at: moment(item.created_at).format("YYYY-MM-DD")
+        }
+      })
+      res.json(dataFormatted)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 })
 
 router.post('/', (req, res, next) => {
