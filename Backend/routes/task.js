c04091c31afd2c7446c339e63fced56835074e5d
Victoriano <mytacam78@gmail.com>
2023-07-09 07:02:52 -0600

    👽️ update api

--- Backend/routes/task.js
+++ Backend/routes/task.js
@@ -1,12 +1,12 @@
-const { Router, response } = require('express')
-const moment = require('moment')
+const { Router } = require('express')
 const router = Router()
+const { get, run }  = require('./../services/db')
 
-// data
-// const db  = require('./../services/db')
-const { get }  = require('./../services/db')
+const moment = require('moment')
+// middleware
 const validatorData = require('./../middlewares/validatorData')
 
+// api
 router.get('/', async (req, res, next) => {
   // const taskId = req.query.id
   try {
@@ -17,7 +17,7 @@ router.get('/', async (req, res, next) => {
         id: toDo.id,
         title: toDo.title,
         description: toDo.description,
-        isdone: Boolean(toDo.isDone),
+        isdone: Boolean(toDo.isdone),
         created_at: moment(toDo.created_at).format("YYYY-MM-DD")
       }
     })
@@ -60,16 +60,102 @@ router.get('/', async (req, res, next) => {
   // }
 })
 
-router.post('/', (req, res, next) => {
-  console.log('Post');
+router.post('/', async (req, res, next) => {
+  try {
+    const { title, description, isdone, created_at } = req.body
+    const data = await run("INSERT INTO tasks (title, description, isdone, created_at) VALUES (?, ?, ?, ?)", [title, description, isdone, created_at])
+    console.log(data.lastID);
+
+    res.status(200).json({
+      message: "To-do created succesfully",
+      // toDo: {}
+      toDo: {
+        id: data.lastID,
+        title,
+        description,
+        isdone: false,
+        created_at: moment(created_at).format("YYYY-MM-DD")
+      }
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "error en el servidor", 
+      error
+    })
+  }
 })
 
-router.patch('/', (req, res, next) => {
-  console.log('patch');
+router.patch('/:id', async (req, res, next) => {
+  try {
+    console.log('patch');
+    const { id } = req.params
+    const toDo = await get("SELECT * FROM tasks WHERE ID = ?", [id])
+    if (toDo.length === 0) {
+      return res
+        .status(404)
+        .json({ message: `el ID no se encuentra en la db`})
+    }
+    let { title, description, isdone, created_at } = req.body
+    
+    if (typeof isdone == 'undefined') {
+      isdone = toDo[0].isdone
+    }
+
+    if (typeof title == 'undefined') {
+      title = toDo[0].title
+    }
+
+    if (typeof description == 'undefined') {
+      description = toDo[0].description
+    }
+
+    const isDoneNumber = Number(isdone)
+    await run(`UPDATE tasks SET title = ?, description = ?, created_at = ?`, [title, description, created_at, isDoneNumber, id, ])
+
+    res.status(200).json({
+      message: `To-do updated succesfully`,
+      toDo: {
+        id: data.lastID,
+        title,
+        description,
+        isdone: false,
+        created_at: moment(created_at).format("YYYY-MM-DD")
+      }
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ 
+      message: "error en el sevidor", error
+    })
+  }
 })
 
-router.delete('/', (req, res, next) => {
-  console.log('delete');
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const toDo = await get("SELECT * FROM tasks WHERE id = ?", [id])
+
+    if (toDo.length === 0) {
+      return res
+        .status(404)
+        .json({ message: `el ID no se encuentra en la db`})
+    }
+    await run("DELETE FROM tasks WHERE id = ?", [id])
+    res.status(200).json({
+      message: `To-do deleted successfully`,
+      toDo: {
+        id: toDo[0].id,
+        title: toDo[0].title,
+        description: toDo[0].description,
+        isdone: Boolean(toDo[0].isdone),
+        created_at: moment(created_at).format("YYYY-MM-DD")
+      }
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error en el servidor", error })
+  }
 })
 
 module.exports = router
--- Backend/services/db.js
+++ Backend/services/db.js
@@ -1,9 +1,25 @@
 const sqlite3 = require('sqlite3')
 const path = require('path')
 
-const dbPath = path.resolve(process.cwd(), 'services', 'tasking.sqlite')
+const dbPath = path.resolve(process.cwd(), 'db', 'tasking.sqlite')
 
 const db = new sqlite3.Database(dbPath)
+
+// post
+const run = (query, params = []) => {
+  return new Promise((res, rej) => {
+    db.run(query, params, function (error) {
+      if (error) {
+        return rej(error)
+      }
+      return res({
+        status: true,
+        lastID: this.lastID,
+        changes: this.changes
+      })
+    })
+  })
+}
 
 const get = (query, params = []) => {
   return new Promise((res, rej) => {
@@ -19,14 +35,22 @@ const get = (query, params = []) => {
 
 const initDB = async () => {
   try {
-    await db.run('CREATE TABLE IF NOT EXISTS tasks(id INTERGER PRIMARY KEY, title TEXT, description TEXT, isdone BOOLEAN, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)')
+    await run(`CREATE TABLE IF NOT EXISTS tasks(
+      id INTERGER PRIMARY KEY, 
+      title TEXT, 
+      description TEXT, 
+      isdone BOOLEAN, 
+      created_at DATETIME DEFAULT 0,
+      CURRENT_TIMESTAMP NOT NULL)`)
     console.log('Tabla tasks Cargadas');
   } catch (error) {
-    throw new Error(error)
+    // throw new Error(error)
+    console.log(error);
   }
 }
 
 module.exports = {
   get,
+  run,
   initDB
 }
