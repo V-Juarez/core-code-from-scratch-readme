0fe3bb7cd062e6b562bff76f0d18eeb9c0f524d4
Victoriano <mytacam78@gmail.com>
2023-07-12 21:14:37 -0700

    🚚 Create task

--- Frontend/src/pages/TaskForm.jsx
+++ Frontend/src/pages/TaskForm.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
+import axios from 'axios'
 
 function TaskForm() {
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
-
+  const [isdone, setIsdone] = useState(false)
+  const [created_at, setCreated_at] = useState('')
 
   const handleSubmite = async (e) => {
     e.preventDefault()
+    console.log(title, description);
 
-    const res = await fetch('http://localhost:4000/api/tasks', {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        description
-      })
+    // const res = await fetch('http://localhost:4000/api/tasks', {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     title,
+    //     description
+    //   }),
+    //   headers: {
+    //     'Content-type': 'application/json'
+    //   }
+    // })
+
+    // conect api
+    const res = await axios.post('http://localhost:4000/api/task', {
+      title,
+      description,
+      isdone,
+      created_at
     })
     console.log(res);
+    e.target.reset()
   }
 
   return (
@@ -33,6 +48,18 @@ function TaskForm() {
           className="block py-2 px-3 mb-4 w-full text-black"
           onChange={(e) => setDescription(e.target.value)}
         ></textarea>
+        <input
+          type="text"
+          placeholder="false"
+          className="block py-2 px-3 mb-4 w-full text-black"
+          onChange={(e) => setCreated_at(e.target.value)} 
+        />
+        <input
+          type="date"
+          placeholder="date"
+          className="block py-2 px-3 mb-4 w-full text-black"
+          onChange={(e) => setCreated_at(e.target.value)} 
+        />
         <button>save</button>
       </form>
     </div>
