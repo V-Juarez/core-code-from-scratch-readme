346b8dea929c0f55185167e9d9249b820e13e818
Victoriano <mytacam78@gmail.com>
2023-07-14 07:31:10 -0700

    ♻️ refactor code

--- Frontend/src/components/TaskCard.jsx
+++ Frontend/src/components/TaskCard.jsx
@@ -1,18 +1,43 @@
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { updateTask } from "../api/tasks";
 
 function TaskCard({ task }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div
       className="bg-zinc-950 hover:cursor-pointer hover:bg-gray-500"
       onClick={() => {
-        navigate(`/tasks/${task.id}`)
+        navigate(`/tasks/${task.id}`);
       }}
     >
-      <h2>{task.title}</h2>
-      <p>{task.description}</p>
-      <p>{task.isDone}</p>
-      <span>{task.created_at}</span>
+      <div className="flex justify-between">
+        <h2 className="font-bold text-2xl">{task.title}</h2>
+        <button
+          onClick={async (e) => {
+            e.stopPropagation();
+            const res = await updateTask(task.id, {
+              completed: !task.completed,
+            });
+            if (res.status === 200) {
+              window.location.reload()
+            }
+          }}
+        >
+          <svg
+            className={`w-6 h-6 ${task.completed ? "text-green-900" : ""}`}
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M4.5 12.75l6 6 9-13.5" strokeLinecap="round" />
+          </svg>
+        </button>
+      </div>
+      <p className="text-slate-400">{task.description}</p>
+      <p>{task.isDone ? 'Completada' : 'Pendiente'}</p>
+      <span className="text-slate-200">{task.created_at}</span>
     </div>
   );
 }
--- Frontend/src/components/TaskList.jsx
+++ Frontend/src/components/TaskList.jsx
@@ -1,7 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import TaskCard from "./TaskCard"
 
 function TaskList({ tasks }) {
-  
+  const [completedTask, setCompletedTask] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('')
+        const data = await res.json()
+
+        const taskDefaultStatus = data.map(task => ({
+          ...task,
+          isDone: false,
+        }))
+
+        setCompletedTask(taskDefaultStatus)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div className="grid grid-cols-3 gap-4">
       {
